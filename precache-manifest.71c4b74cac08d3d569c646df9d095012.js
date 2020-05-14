b7485fc1a45ea376da674cf1ea83c86c6f05a4e6
stefanv14 <vasilijevic032@gmail.com>
2020-05-14 10:34:42 +0200

    Updates

---
+++ precache-manifest.71c4b74cac08d3d569c646df9d095012.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c16da1a97f0d4ecae4587e38f08e71",
+    "url": "/it-web-page-pratice/index.html"
+  },
+  {
+    "revision": "048180731149995a8c3b",
+    "url": "/it-web-page-pratice/static/css/main.37449bef.chunk.css"
+  },
+  {
+    "revision": "5fde7e61666335beaabc",
+    "url": "/it-web-page-pratice/static/js/2.3de634e3.chunk.js"
+  },
+  {
+    "revision": "048180731149995a8c3b",
+    "url": "/it-web-page-pratice/static/js/main.4521fd2f.chunk.js"
+  },
+  {
+    "revision": "e4ddb0d71768af2d1eba",
+    "url": "/it-web-page-pratice/static/js/runtime-main.26af5b13.js"
+  },
+  {
+    "revision": "5adb715bbbbfabae9e67053067d372eb",
+    "url": "/it-web-page-pratice/static/media/39BFAC_0_0.5adb715b.ttf"
+  },
+  {
+    "revision": "506b18f069f1ea36608ce100aaa4d656",
+    "url": "/it-web-page-pratice/static/media/39BFAC_1_0.506b18f0.ttf"
+  },
+  {
+    "revision": "5f2aa2aedfaee2f7f358ae948955270f",
+    "url": "/it-web-page-pratice/static/media/B22_IT__header hamburger.5f2aa2ae.svg"
+  },
+  {
+    "revision": "d5c1a0900b6de1112a88d72618cfb9b6",
+    "url": "/it-web-page-pratice/static/media/B22_IT__header login.d5c1a090.svg"
+  },
+  {
+    "revision": "45c54049ead3ff5be016b332604268a5",
+    "url": "/it-web-page-pratice/static/media/GetQuoteIcon.45c54049.svg"
+  },
+  {
+    "revision": "320a73ab3e818006075943dbbfe0f297",
+    "url": "/it-web-page-pratice/static/media/IT-white.320a73ab.svg"
+  },
+  {
+    "revision": "0b0be889693c0bef1752e60d983968bb",
+    "url": "/it-web-page-pratice/static/media/ITClubIcon.0b0be889.svg"
+  },
+  {
+    "revision": "17c053ac423114acfd82306accc37b2b",
+    "url": "/it-web-page-pratice/static/media/JoinITClub.17c053ac.svg"
+  },
+  {
+    "revision": "21d395725322a3670e773cf73f3a5d5d",
+    "url": "/it-web-page-pratice/static/media/NewsletterIcon.21d39572.svg"
+  },
+  {
+    "revision": "fedb2935a96b8198b5c9f14fcccfa2de",
+    "url": "/it-web-page-pratice/static/media/OurClientsImg.fedb2935.png"
+  },
+  {
+    "revision": "8c46c90d6c2919ca32798b828a3e4d35",
+    "url": "/it-web-page-pratice/static/media/S1.8c46c90d.svg"
+  },
+  {
+    "revision": "30ac1873758516e67c58399e7e466cb5",
+    "url": "/it-web-page-pratice/static/media/S2.30ac1873.svg"
+  },
+  {
+    "revision": "6e255c6b00ab783a4e536496ee6c480c",
+    "url": "/it-web-page-pratice/static/media/S3.6e255c6b.svg"
+  },
+  {
+    "revision": "fbae8a5fc78d1263937c778d1e70fd88",
+    "url": "/it-web-page-pratice/static/media/S4.fbae8a5f.svg"
+  },
+  {
+    "revision": "e1f3c2d97e7ec28676596b708c5bb56f",
+    "url": "/it-web-page-pratice/static/media/S5.e1f3c2d9.svg"
+  },
+  {
+    "revision": "e6ed7b927930aecc71996583ccc2a218",
+    "url": "/it-web-page-pratice/static/media/S6.e6ed7b92.svg"
+  },
+  {
+    "revision": "fd69582c29d3d16d687dd72ef4d13999",
+    "url": "/it-web-page-pratice/static/media/ServicesBackground.fd69582c.svg"
+  },
+  {
+    "revision": "84f6e73d47ea13259448bb2d6ed66521",
+    "url": "/it-web-page-pratice/static/media/ShareIcon.84f6e73d.svg"
+  },
+  {
+    "revision": "fbf596bc427d27c67e5580c40b6d032c",
+    "url": "/it-web-page-pratice/static/media/Subscribe.fbf596bc.svg"
+  },
+  {
+    "revision": "ad03a36dd3610a0941ec0d101a576eec",
+    "url": "/it-web-page-pratice/static/media/WhyNot.ad03a36d.svg"
+  },
+  {
+    "revision": "2c2615f9c532bb2ffc8f04dbc8319bac",
+    "url": "/it-web-page-pratice/static/media/blog.2c2615f9.png"
+  },
+  {
+    "revision": "b74d2b9bc2d566abbdbe83fbb564ab9e",
+    "url": "/it-web-page-pratice/static/media/careers.b74d2b9b.svg"
+  },
+  {
+    "revision": "f1619c6d7115e1d53baf359352a3af95",
+    "url": "/it-web-page-pratice/static/media/clients-hand.f1619c6d.svg"
+  },
+  {
+    "revision": "c6e3fcdd472fac7a51a652b3b8dc835c",
+    "url": "/it-web-page-pratice/static/media/company.c6e3fcdd.svg"
+  },
+  {
+    "revision": "18594288db9c4d9d4f2b0cde49efbdfe",
+    "url": "/it-web-page-pratice/static/media/download-icon.18594288.svg"
+  },
+  {
+    "revision": "6c5fcb26512ab5dc8b0cbb3a34ef76a6",
+    "url": "/it-web-page-pratice/static/media/downloadIcon.6c5fcb26.svg"
+  },
+  {
+    "revision": "13169c27598c4b29b780e8a609870591",
+    "url": "/it-web-page-pratice/static/media/headerBack.13169c27.png"
+  },
+  {
+    "revision": "39582529b7aeb11beb44308a5a4c05e3",
+    "url": "/it-web-page-pratice/static/media/headerBackground.39582529.svg"
+  },
+  {
+    "revision": "b6a9a87d2cac44227236bb97d079f8f8",
+    "url": "/it-web-page-pratice/static/media/headerSectionLogo.b6a9a87d.svg"
+  },
+  {
+    "revision": "c5796980147eb01faccb40c9d5e7cf4d",
+    "url": "/it-web-page-pratice/static/media/logo.c5796980.svg"
+  },
+  {
+    "revision": "3e7220554af5c375121b1f03ba5625b2",
+    "url": "/it-web-page-pratice/static/media/loyality.3e722055.svg"
+  },
+  {
+    "revision": "aefdcc0171c4ff5d63de8e2185cca599",
+    "url": "/it-web-page-pratice/static/media/nicheBackground.aefdcc01.svg"
+  },
+  {
+    "revision": "5cf75cfd6ff302b9e2bfffeb02f33f36",
+    "url": "/it-web-page-pratice/static/media/partners.5cf75cfd.svg"
+  },
+  {
+    "revision": "0cba2fc70e8e3b091f8c5888f7e0c0a1",
+    "url": "/it-web-page-pratice/static/media/team.0cba2fc7.svg"
+  },
+  {
+    "revision": "8ef7dddcb0bfd7fcbdb3fc39924e882e",
+    "url": "/it-web-page-pratice/static/media/teams1.8ef7dddc.svg"
+  },
+  {
+    "revision": "8642792cadcfe20dd135d8f5eff33ffa",
+    "url": "/it-web-page-pratice/static/media/teams2.8642792c.svg"
+  },
+  {
+    "revision": "475e40d9166cc95a64c67eb9080f6581",
+    "url": "/it-web-page-pratice/static/media/teamsBackground.475e40d9.svg"
+  },
+  {
+    "revision": "8a31bbc1a09d7d6e08b114490880cd45",
+    "url": "/it-web-page-pratice/static/media/telefon-mockup.8a31bbc1.png"
+  },
+  {
+    "revision": "baf62d36a56da35504d10c152ce301b2",
+    "url": "/it-web-page-pratice/static/media/value.baf62d36.svg"
+  },
+  {
+    "revision": "053cf30f605cfb81fb62a2cc74629cb5",
+    "url": "/it-web-page-pratice/static/media/video.053cf30f.svg"
+  }
+]);
